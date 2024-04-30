4d38acea9e2d1e88b41ff2e86fe096fdc0667851
Janaina Tavares <78567230+tavaresjana@users.noreply.github.com>
2024-04-30 15:52:16 -0300

    Aula2904: Aula de Deploy remoto de Banco de Dados e Triggers

--- aula2904/script2904_local.sql
+++ aula2904/script2904_local.sql
@@ -4,3 +4,23 @@ insert into dependente(cod_emp, nome, dt_nascimento, sexo) values(17,'LETÍCIA I
 
 rollback;
 commit;
+
+/*todo gatilho pede um delimiter
+cria um gatilho, quando excluir um empregado. gatilho é acionado depois de deletar da tab empregado */
+
+drop trigger gatilho_exclusao_emp;
+
+delimiter &&
+create trigger gatilho_exclusao_emp before delete
+on empregado
+for each row 
+begin
+	delete from dependente dep where dep.cod_emp = old.cod_emp;
+end
+&&
+
+/* */
+start transaction;
+delete from empregado where cod_emp=17;
+rollback;
+commit;
